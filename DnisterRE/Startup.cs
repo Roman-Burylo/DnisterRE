d9981d758c06a85bbedf158528b32e4ac2b6fc24
stetskivostap@gmail.com <stetskivostap@gmail.com>
2020-03-16 20:32:52 +0200

    commit

--- DnisterRE/Startup.cs
+++ DnisterRE/Startup.cs
@@ -29,7 +29,7 @@ namespace DnisterRE
             {
                 configuration.RootPath = "ClientApp/dist";
             });
-
+        
             services.AddDbContext<DnisterDbContext>(item => item.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddTransient<IUnitOfWork, UnitOfWork>(provider =>
                new UnitOfWork(provider.GetRequiredService<DnisterDbContext>()));
