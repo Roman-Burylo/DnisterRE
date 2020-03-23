4d6e77a191423f61676b79d6ecc24bfd307ecff4
AHAPXICT <roman.kalitovskyy@gmail.com>
2020-03-23 10:28:54 +0200

    toolbar

--- DnisterRE/ClientApp/src/app/app.component.ts
+++ DnisterRE/ClientApp/src/app/app.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { MatBottomSheet, MatFormField } from '@angular/material'
+import { LoginFormComponent } from './login-form/login-form.component'
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+export interface DialogData {
+  login: string;
+  password: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
+
 export class AppComponent {
   title = 'app';
+
+  constructor(private bottomShit: MatBottomSheet) {
+
+  }
+
+  openLoginDialog() {
+    this.bottomShit.open(LoginFormComponent);
+    console.log('#')
+  }
 }
