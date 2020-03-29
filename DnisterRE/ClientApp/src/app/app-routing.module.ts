7deef0ab295cd69bac2f5e2e8128f39a64af72f6
AHAPXICT <roman.kalitovskyy@gmail.com>
2020-03-29 20:08:20 +0300

    organization

---
+++ DnisterRE/ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+// src/app/app-routing.module.ts
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UsersPageComponent } from './users-page/users-page.component';
+
+const routes: Routes = [
+    { path: 'error', component: ErrorComponent },
+    {
+        path: 'api', component: OrganizationComponent, children: [
+            { path: 'users', component: UsersPageComponent }
+        ]
+    },
+    { path: '', component: HomePageComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- DnisterRE/ClientApp/src/app/app.module.ts
+++ DnisterRE/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, ErrorHandler } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -12,16 +12,32 @@ import {
   MatFormField,
   MatInput
 } from '@angular/material';
+
+import { MatDialogModule } from '@angular/material';
+// import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog'
+
 import { MatBottomSheetModule } from '@angular/material/bottom-sheet'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { LoginFormComponent } from './login-form/login-form.component'
 import { from } from 'rxjs'
+
+import { ErrorComponent } from './error/error.component'
+import { GlobalErrorHandler } from './global-error-handler.service';
+import { AppRoutingModule } from './app-routing.module';
+import { NewsComponent } from './components/news/news.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { UsersPageComponent } from './users-page/users-page.component'
+import { EditFromComponent } from './edit-from/edit-from.component';
+import { AdduserFormComponent } from './adduser-form/adduser-form.component';
 
 @NgModule({
   imports: [
     MatFormField,
     MatInput,
     MatLabel,
+    AppRoutingModule,
+    // MatDialog
   ],
   exports: [
     MatFormField,
@@ -30,6 +46,12 @@ import { from } from 'rxjs'
   ],
   declarations: [
     LoginFormComponent,
+    NewsComponent,
+    ErrorComponent,
+    OrganizationComponent,
+    HomePageComponent,
+    UsersPageComponent,
+    EditFromComponent
   ]
 }) export class MaterialModule { };
 
@@ -37,6 +59,12 @@ import { from } from 'rxjs'
   declarations: [
     AppComponent,
     LoginFormComponent,
+    ErrorComponent,
+    OrganizationComponent,
+    HomePageComponent,
+    UsersPageComponent,
+    EditFromComponent,
+    AdduserFormComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -46,12 +74,15 @@ import { from } from 'rxjs'
     MatButtonModule,
     BrowserAnimationsModule,
     MatBottomSheetModule,
-    // MatFormField,
-    // MatInput,
-    // MatLabel,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatDialogModule,
   ],
-  providers: [],
-  entryComponents: [LoginFormComponent],
+  providers: [
+    { provide: ErrorHandler, useClass: GlobalErrorHandler }
+  ],
+  entryComponents: [LoginFormComponent, EditFromComponent, AdduserFormComponent],
   bootstrap: [AppComponent],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA,
